a79c1e30a2fcbcc747c4e44153029575cea9b17c
Glenn Rudge <glennrudge@outlook.com>
2023-05-05 13:44:04 -0400

    Create docker-compose.yml for postgresql install prisma ORM, update app.module.ts, auth.controller.ts, auth.module.ts, auth.service.ts

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(): object {
+    return this.authService.signup();
+  }
+  @Post('signin')
+  signin(): object {
+    return this.authService.login();
+  }
+  @Get()
+  foo(): string {
+    return 'bar';
+  }
+}
