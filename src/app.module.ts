ce67360ca45bcec37591729bdc479446ac71c194
Glenn Rudge <glennrudge@outlook.com>
2023-05-05 15:52:14 -0400

    Create migration.sql prisma.module.ts and prisma.service.ts, update .env app.module.ts docker-compose.yml package-lock.jsonand schema.prisma.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaNoSpecService } from './prisma--no-spec/prisma--no-spec.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule],
+  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaNoSpecService],
 })
 export class AppModule {}
